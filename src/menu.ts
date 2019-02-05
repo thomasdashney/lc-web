455f6af699904636a258fd933af5630d4a41cab9
Thomas Dashney <tdashney@theworkinggroup.ca>
2019-02-05 02:08:51 -0500

    improve links

--- src/menu.ts
+++ src/menu.ts
@@ -6,25 +6,25 @@ interface IMenuItem {
 
 export const MENU_ITEMS: IMenuItem[] = [
   { to: "/music", name: "Music" },
+  { to: "/connect", name: "Connect" },
   {
     to: "https://www.songkick.com/artists/8158333-lost-cousins/calendar",
-    name: "Shows",
+    name: "Shows↗︎",
+    external: true
+  },
+  {
+    to: "https://www.merchmrkt.com/merchmrkt/vendor/lostcousins",
+    name: "Merch↘︎",
     external: true
   },
   {
     to: "https://www.instagram.com/lostcousinsband",
-    name: "Photos",
+    name: "Photos↗︎",
     external: true
   },
   {
     to: "https://www.youtube.com/lostcousinsband",
-    name: "Videos",
-    external: true
-  },
-  { to: "/connect", name: "Connect" },
-  {
-    to: "https://www.merchmrkt.com/merchmrkt/vendor/lostcousins",
-    name: "Merch",
+    name: "Videos↘︎",
     external: true
   }
 ];
